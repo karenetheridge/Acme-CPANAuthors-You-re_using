e8b06a5663a989f54c31b85081660ea48f24d495
Vincent Pit <vince@profvince.com>
2010-01-03 11:07:54 +0100

    This is 0.03

--- lib/Acme/CPANAuthors/You/re_using.pm
+++ lib/Acme/CPANAuthors/You/re_using.pm
@@ -15,13 +15,13 @@ Acme::CPANAuthors::You::re_using - We are the CPAN authors that have written the
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
 our $VERSION;
 BEGIN {
- $VERSION = '0.02';
+ $VERSION = '0.03';
 }
 
 =head1 SYNOPSIS
