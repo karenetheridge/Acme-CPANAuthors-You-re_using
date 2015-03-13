8b040d34383cac63412dad5910c4e5e51c9dd629
Vincent Pit <vince@profvince.com>
2015-03-13 12:29:32 -0300

    This is 0.07

--- lib/Acme/CPANAuthors/You/re_using.pm
+++ lib/Acme/CPANAuthors/You/re_using.pm
@@ -14,13 +14,13 @@ Acme::CPANAuthors::You::re_using - We are the CPAN authors that have written the
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
 our $VERSION;
 BEGIN {
- $VERSION = '0.06';
+ $VERSION = '0.07';
 }
 
 =head1 SYNOPSIS
